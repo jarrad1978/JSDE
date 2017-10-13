f738344132f4dd0ab3e99494859b3d3e0f27d013
Jarrad <jarradglover@gmail.com>
2017-10-12 23:08:05 -0500

    -Prod webpack config / minification -config local / dist server -added toggle for mock api

---
+++ buildScripts/build.js
@@ -0,0 +1,29 @@
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import chalk from 'chalk';
+
+process.env.NODE_ENV = 'production';
+
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'))
+webpack(webpackConfig).run((err, stats) => {
+  if(err){
+    console.log(chalk.red(err));
+    return 1;
+  }
+
+  const jsonStats = stats.toJSON();
+  if(jsonStats.hasErrors()){
+    return jsonStats.errors.map(error => console.log(chalk.red(error)));
+  }
+
+  if(jsonStats.hasWarnings()){
+    console.log(chalk.yellow('Webpack generated the following warnings: '));
+    jsonStats.warnings.map(error => console.log(chalk.yellow(error)));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  //if we got this far it worked
+  console.log(chalk.green('Your app has been built for production and written to /dist!'));
+  return 0;
+});
