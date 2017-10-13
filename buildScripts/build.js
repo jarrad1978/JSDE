c3f983dd7fc9bb4b701bb04e31dafe39c08c1cd9
Jarrad <jarradglover@gmail.com>
2017-10-12 23:12:42 -0500

    removed reference to webpack for prod build and removed linting warnings for console logging.

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -4,26 +4,26 @@ import chalk from 'chalk';
 
 process.env.NODE_ENV = 'production';
 
-console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'))
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...')) //eslint-disable-line no-console
 webpack(webpackConfig).run((err, stats) => {
   if(err){
-    console.log(chalk.red(err));
+    console.log(chalk.red(err)); //eslint-disable-line no-console
     return 1;
   }
 
   const jsonStats = stats.toJSON();
   if(jsonStats.hasErrors()){
-    return jsonStats.errors.map(error => console.log(chalk.red(error)));
+    return jsonStats.errors.map(error => console.log(chalk.red(error))); //eslint-disable-line no-console
   }
 
   if(jsonStats.hasWarnings()){
-    console.log(chalk.yellow('Webpack generated the following warnings: '));
-    jsonStats.warnings.map(error => console.log(chalk.yellow(error)));
+    console.log(chalk.yellow('Webpack generated the following warnings: ')); //eslint-disable-line no-console
+    jsonStats.warnings.map(error => console.log(chalk.yellow(error))); //eslint-disable-line no-console
   }
 
-  console.log(`Webpack stats: ${stats}`);
+  console.log(`Webpack stats: ${stats}`); //eslint-disable-line no-console
 
   //if we got this far it worked
-  console.log(chalk.green('Your app has been built for production and written to /dist!'));
+  console.log(chalk.green('Your app has been built for production and written to /dist!')); //eslint-disable-line no-console
   return 0;
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,12 +13,7 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    //Eliminate duplicate packages when generating bundle
-    new webpack.optimize.DedupePlugin(),
-    //Minify JS
-    new webpack.optimize.UglifyJsPlugin()
-  ],
+  plugins: [],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
