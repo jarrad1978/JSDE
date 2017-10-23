dbf58e4f31d177bcc779f8d411c224e92c8c2e9d
Jarrad <jarradglover@gmail.com>
2017-10-23 14:24:07 -0500

    removed reference to webpack for prod build and removed linting warnings for console logging.

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -1,29 +1,32 @@
+/*eslint-disable no-console */
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.prod';
 import chalk from 'chalk';
 
 process.env.NODE_ENV = 'production';
 
-console.log(chalk.blue('Generating minified bundle for production. This will take a moment...')) //eslint-disable-line no-console
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
+
 webpack(webpackConfig).run((err, stats) => {
   if(err){
-    console.log(chalk.red(err)); //eslint-disable-line no-console
+    console.log(chalk.red(err));
     return 1;
   }
 
-  const jsonStats = stats.toJSON();
-  if(jsonStats.hasErrors()){
-    return jsonStats.errors.map(error => console.log(chalk.red(error))); //eslint-disable-line no-console
+  const jsonStats = stats.toJson();
+
+  if(jsonStats.hasErrors){
+    return jsonStats.errors.map(error => console.log(chalk.red(error)));
   }
 
-  if(jsonStats.hasWarnings()){
-    console.log(chalk.yellow('Webpack generated the following warnings: ')); //eslint-disable-line no-console
-    jsonStats.warnings.map(error => console.log(chalk.yellow(error))); //eslint-disable-line no-console
+  if(jsonStats.hasWarnings){
+    console.log(chalk.yellow('Webpack generated the following warnings: '));
+    jsonStats.warnings.map(error => console.log(chalk.yellow(error)));
   }
 
-  console.log(`Webpack stats: ${stats}`); //eslint-disable-line no-console
+  console.log(`Webpack stats: ${stats}`);
 
   //if we got this far it worked
-  console.log(chalk.green('Your app has been built for production and written to /dist!')); //eslint-disable-line no-console
+  console.log(chalk.green('Your app has been built for production and written to /dist!'));
   return 0;
 });
